d1274d405fc71edb58b95f8b533d3431731aa2e0
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-09-15 21:39:15 +0200

    Redirect user index route to log in

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.redirect("/user/log-in");
 });
 
 router.get("/sign-up", userController.signUpGet);
